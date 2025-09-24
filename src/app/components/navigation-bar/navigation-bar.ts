6f0ece20be4fb75603ce237aceaa0d15d850cfac
ienjir <info@ienjir.dev>
2025-09-24 07:26:44 +0200

    Updated a bit of the nav bar but need to add new sidebar for mobile

--- src/app/components/navigation-bar/navigation-bar.ts
+++ src/app/components/navigation-bar/navigation-bar.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import {TranslocoPipe} from '@jsverse/transloco';
+import {RouterLink} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'NavigationBar',
   imports: [
-    TranslocoPipe
+    TranslocoPipe,
+    RouterLink,
+    NgOptimizedImage
   ],
   templateUrl: './navigation-bar.html',
   styleUrl: './navigation-bar.scss'
@@ -15,4 +19,11 @@ export class NavigationBar {
   toggleSidebar() {
     this.openSidebar = !this.openSidebar;
   }
+
+  navigationItems: {NavigationItem: string, NavigationLink: string}[] = [
+    {NavigationItem: "navbar.home", NavigationLink: "/"},
+    {NavigationItem: "navbar.aboutMe", NavigationLink: "/uebermich"},
+    {NavigationItem: "navbar.projects", NavigationLink: "/projekte"},
+    {NavigationItem: "navbar.contact", NavigationLink: "/kontakt"},
+  ]
 }
