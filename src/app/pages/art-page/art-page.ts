62e45584c94f3d82407275711b9b8e1e502ece21
ienjir <info@ienjir.dev>
2025-10-02 12:08:24 +0200

    Added backup jk

--- src/app/pages/art-page/art-page.ts
+++ src/app/pages/art-page/art-page.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {ArtPreview, ArtPreviewItem} from "@components/art-preview/art-preview";
 
 @Component({
   selector: 'ArtPage',
-  imports: [],
+  imports: [
+    ArtPreview
+  ],
   templateUrl: './art-page.html',
   styleUrl: './art-page.scss'
 })
 export class ArtPage {
-
+  ArtPreviews: ArtPreviewItem[] = [
+    {id: 1, Title: "Test1", Alt: "Test1", Link: "/images/schildkröte.jpg", Label: "Test1"},
+    {id: 2, Title: "Test1", Alt: "Test1", Link: "/images/ding-dong3.jpg", Label: "Test1"},
+    {id: 3, Title: "Test1", Alt: "Test1", Link: "/images/windBlumeWinter.jpg", Label: "Test1"}
+  ]
 }
