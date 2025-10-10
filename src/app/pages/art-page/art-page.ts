72036afbe41e9ba01267cccdb7e8e33648fef5c9
ienjir <info@ienjir.dev>
2025-10-10 14:15:44 +0200

    Fixed layout and hopefully it is working now

--- src/app/pages/art-page/art-page.ts
+++ src/app/pages/art-page/art-page.ts
@@ -13,6 +13,7 @@ export class ArtPage {
   ArtPreviews: ArtPreviewItem[] = [
     {id: 1, Title: "Test1", Alt: "Test1", Link: "/images/schildkröte.jpg", Label: "Test1"},
     {id: 2, Title: "Test1", Alt: "Test1", Link: "/images/ding-dong3.jpg", Label: "Test1"},
-    {id: 3, Title: "Test1", Alt: "Test1", Link: "/images/windBlumeWinter.jpg", Label: "Test1"}
+    {id: 3, Title: "Test1", Alt: "Test1", Link: "/images/windBlumeWinter.jpg", Label: "Test1"},
+    {id: 4, Title: "Test1", Alt: "Test1", Link: "/images/windBlumeWinter.jpg", Label: "Test1"},
   ]
 }
