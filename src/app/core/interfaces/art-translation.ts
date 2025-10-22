8f5a9ad375e2b69e4ca6b785535ccb56bacd355d
ienjir <info@ienjir.dev>
2025-10-22 14:18:43 +0200

    Added ArtTranslation

---
+++ src/app/core/interfaces/art-translation.ts
@@ -0,0 +1,13 @@
+import {Language} from "@core/interfaces/language";
+
+export interface ArtTranslation {
+  id: number
+  created_at: string
+  updated_at: string
+  art_id: number
+  language_id: number
+  language?: Language
+  title: string
+  description: string
+  text: string
+}
