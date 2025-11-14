178755dfadaaf4850bf7afdc96bf0b1d0249efa9
ienjir <info@ienjir.dev>
2025-11-14 12:44:26 +0100

    Updated toasts for not throwing ssr errors

--- src/app/components/global-toast/toasts-continer.ts
+++ src/app/components/global-toast/toasts-continer.ts
@@ -1,36 +1,41 @@
-import { Component, inject } from '@angular/core';
-import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
+import {Component, inject, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
+import {NgbToastModule} from '@ng-bootstrap/ng-bootstrap';
 import {ToastService} from "@core/services/toast-service/toast-service";
 
 @Component({
   selector: 'app-toasts',
   imports: [NgbToastModule],
   template: `
-    @for (toast of toastService.toasts(); track toast) {
-      <ngb-toast
-        [class]="toast.classname"
-        [autohide]="true"
-        [delay]="toast.delay || 5000"
-        (hidden)="toastService.remove(toast)"
-      >
-        <div class="d-flex justify-content-between align-items-center mb-2">
-          @if (toast.title) {
-            <strong>{{ toast.title }}</strong>
-          }
-          <button
-            type="button"
-            class="btn-close btn-close-white ms-2"
-            style="min-height: unset;"
-            aria-label="Close"
-            (click)="toastService.remove(toast)"
-          ></button>
-        </div>
-        <div>{{ toast.message }}</div>
-      </ngb-toast>
+    @if (isBrowser) {
+      @for (toast of toastService.toasts(); track toast) {
+        <ngb-toast
+          [class]="toast.classname"
+          [autohide]="true"
+          [delay]="toast.delay || 5000"
+          (hidden)="toastService.remove(toast)"
+        >
+          <div class="d-flex justify-content-between align-items-center mb-2">
+            @if (toast.title) {
+              <strong>{{ toast.title }}</strong>
+            }
+            <button
+              type="button"
+              class="btn-close btn-close-white ms-2"
+              style="min-height: unset;"
+              aria-label="Close"
+              (click)="toastService.remove(toast)"
+            ></button>
+          </div>
+          <div>{{ toast.message }}</div>
+        </ngb-toast>
+      }
     }
   `,
-  host: { class: 'toast-container position-fixed top-0 end-0 p-3', style: 'z-index: 1200' },
+  host: {class: 'toast-container position-fixed top-0 end-0 p-3', style: 'z-index: 1200'},
 })
 export class ToastsContainer {
   readonly toastService = inject(ToastService);
+  private readonly platformId = inject(PLATFORM_ID);
+  readonly isBrowser = isPlatformBrowser(this.platformId);
 }
