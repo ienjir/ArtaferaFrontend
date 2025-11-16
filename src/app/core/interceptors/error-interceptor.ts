2b2ac1412b919b8f2772b8fa0d7868536b3a5475
ienjir <info@ienjir.dev>
2025-11-16 15:39:13 +0100

    Added 401 redirect to login which isnt there yet but who cares about that?

--- src/app/core/interceptors/error-interceptor.ts
+++ src/app/core/interceptors/error-interceptor.ts
@@ -2,13 +2,20 @@ import { HttpInterceptorFn } from "@angular/common/http";
 import { catchError, throwError } from "rxjs";
 import { inject } from "@angular/core";
 import { ErrorService } from "@services/error-service/error-service";
+import {Router} from "@angular/router";
 
 export const ErrorInterceptor: HttpInterceptorFn = (req, next) => {
   const errorService = inject(ErrorService);
+  const router = inject(Router)
 
   return next(req).pipe(
     catchError((error) => {
-      errorService.handleHttpError(error);
+      switch (error.status) {
+        case 401:
+          router.navigate(['/login'])
+          break
+      }
+      errorService.handleHttpError(error)
       return throwError(() => error);
     })
   );
