ce963609040d94700c8ca88312f0fe58752b8d53
ienjir <info@ienjir.dev>
2025-11-19 05:41:21 +0100

    Added translation and started skeleton

--- src/app/components/picture-carousel/picture-carousel.ts
+++ src/app/components/picture-carousel/picture-carousel.ts
@@ -1,14 +1,18 @@
-import {Component, input} from '@angular/core';
+import {Component, computed, input} from '@angular/core';
 import {NgbCarouselModule} from '@ng-bootstrap/ng-bootstrap';
 import {NgOptimizedImage} from '@angular/common';
 import {environment} from "@environments/environment";
 import {ArtPicture} from "@interfaces/art-picture.model";
+import {TranslocoPipe} from "@jsverse/transloco";
+import {NgxSkeletonLoaderComponent} from "ngx-skeleton-loader";
 
 @Component({
   selector: 'AF-Picture-Carousel',
   imports: [
     NgbCarouselModule,
     NgOptimizedImage,
+    TranslocoPipe,
+    NgxSkeletonLoaderComponent,
   ],
   templateUrl: './picture-carousel.html',
   styleUrl: './picture-carousel.scss'
@@ -21,11 +25,19 @@ export class PictureCarousel {
   interval = input<number>(4000)
   wrap = input<boolean>(true)
   darkened = input<boolean>(false)
-  pictures = input<ArtPicture[]>()
+  isLoading = input<boolean>(false)
+  pictures = input<ArtPicture[]>([])
+  hasPictures = computed(() => this.pictures()?.length > 1)
   basePictureURL = input(environment.pictureUrl)
 
   pictureUrl(picture: ArtPicture | undefined) {
     const pic = picture?.picture;
     return pic ? `${this.basePictureURL()}/${pic.name}${pic.type}` : '';
+  }
+
+  test() {
+    console.log(this.pictures())
+    console.log(this.hasPictures())
+    console.log(this.pictures()?.length)
   }
 }
